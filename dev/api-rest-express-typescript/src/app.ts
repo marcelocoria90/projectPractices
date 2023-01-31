4d50a9720c89bc4fae4ae9d612270ea6f679d0de
marcelocoria90 <coria.marcelo.90@gmail.com>
2023-01-31 09:37:43 -0300

    Add router

--- dev/api-rest-express-typescript/src/app.ts
+++ dev/api-rest-express-typescript/src/app.ts
@@ -1,10 +1,11 @@
 import 'dotenv/config'
 import express, { application } from 'express'
 import cors from 'cors'
-// import { router } from './routes'
+import { router } from './routes'
 
 const app = express()
 app.use(cors())
+app.use(router)
 
 
 
